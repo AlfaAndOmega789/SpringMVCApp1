06561ed540b656d4321356f085816018193bdac7
tendernesc <122694439+tendernesc@users.noreply.github.com>
2024-02-21 15:37:35 +0300

    init commit

--- src/main/java/ru/kovalia/springcourse/controllers/FirstController.java
+++ src/main/java/ru/kovalia/springcourse/controllers/FirstController.java
@@ -1,14 +1,23 @@
 package ru.kovalia.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPag(){
+    public String helloPag(@RequestParam(value = "name", required = false)String name,
+                           @RequestParam(value = "surname", required = false)String surname,
+                           Model model){
+//        System.out.println("Hello " + name + ", " + surname);
+        model.addAttribute("message", "Hello " + name + ", " + surname);
         return "first/hello";
     }
 
@@ -16,4 +25,28 @@ public class FirstController {
     public String goodByePage(){
         return "first/goodbye";
     }
+    @GetMapping("/calculator")
+    public String mathOperation(@RequestParam(value = "a", required = false)Integer a,
+                                @RequestParam(value = "b", required = false)Integer b,
+                                @RequestParam(value = "action", required = false)String action,
+                                Model model){
+        String str = null;
+
+        if(action.equals("multiplication")){
+            str = String.valueOf(a * b );
+        }
+        if(action.equals("addition")){
+            str = String.valueOf(a + b );
+        }
+        if(action.equals("subtraction")){
+            str = String.valueOf(a - b );
+        }
+        if(action.equals("division")){
+            str = String.valueOf( (double) a / (double) b);
+        }
+        model.addAttribute("message", "Result operation: " + str);
+
+        return "first/calculator";
+    }
+
 }
